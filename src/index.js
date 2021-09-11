90933a7ce749d8f4daa184ad47bd3980ab92dbb5
Kim Litjens <Kimlitjens4@gmail.com>
2021-09-11 15:24:28 +0200

    @env variables

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import reportWebVitals from './reportWebVitals'
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Tournament Pool</h1>
-    {/* <App /> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 )
