5c67c1d8cd9ad3866c8db4b18f5ae8bf1a9adab5
Kim Litjens <kimlitjens4@gmail.com>
2021-12-12 19:29:37 +0100

    add goal scorers

--- src/pages/match/match.js
+++ src/pages/match/match.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import useAxios from '../../utils/hooks/useAxios'
-
+import { Link } from "react-router-dom";
 
 export default function Match() {
     const { match_id } = useParams();
@@ -13,6 +13,9 @@ export default function Match() {
     });
 
     const data = response?.data
+    const homeTeamId = data?.home_team?.team_id
+    const awayTeamId = data?.away_team?.team_id
+
     console.log(data)
     return (
         <div >
@@ -26,15 +29,49 @@ export default function Match() {
                             <h3>Group: {data.group.group_name}</h3>
                         </div>
                         <div className="flex">
-                            <h2 >{data.home_team.name}</h2>
+                            <Link to={`/clubs/${data.home_team.name}/${data.home_team.team_id}`}
+                                className="flex "
+                                target="_blank"
+                            >
+                                <h2 >{data.home_team.name}</h2>
+                                <img
+                                    src={data.home_team.logo}
+                                    alt="Home Team Logo"
+                                    className="w-8 h-8 mx-2"
+                                />
+                            </Link>
                             <h2 className="mx-4">{data.stats.ft_score}</h2>
-                            <h2>{data.away_team.name}</h2>
+                            <Link to={`/clubs/${data.home_team.name}/${data.home_team.team_id}`}
+                                className="flex "
+                                target="_blank"
+                            >
+                                <img
+                                    src={data.away_team.logo}
+                                    alt="Away Team Logo"
+                                    className="w-8 h-8 mx-2"
+                                />
+                                <h2>{data.away_team.name}</h2>
+                            </Link>
                         </div>
-                        <div>
-                            {data.match_events.filter(event => event.type === "goal").map(event =>
-                                <p>{event.player_name} - {event.result}</p>
-                            )}
+                        <div className="flex">
+                            <div>
+                                {data.match_events.filter(event =>
+                                    event.type === "goal" && event.team_id === homeTeamId).map(event =>
+                                        <p> {event.minute}' {event.player_name.split(',').reverse().join(' ')}: {event.result}</p>
+                                    )}
+                            </div>
+                            <div className="w-16">
+
+                            </div>
+
+                            <div>
+                                {data.match_events.filter(event => event.type === "goal" && event.team_id === awayTeamId).map(event =>
+                                    <p> {event.minute}' {event.player_name.split(',').reverse().join(' ')}: {event.result}</p>
+                                )}
+                            </div>
                         </div>
+
+
 
                     </div>}
             </div>
